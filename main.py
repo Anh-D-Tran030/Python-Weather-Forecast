2737c06064e9ccf19d068b5806c3185cb89e82ea
Anh-D-Tran030 <Anh.D.Tran-2@student.uts.edu.au>
2024-12-25 04:20:07 +1100

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,20 @@ import requests
 from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, 
                              QLineEdit, QPushButton, QVBoxLayout)
 from PyQt5.QtCore import Qt
+
+class weatherApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.city_label = QLabel("Enter city name: ", self )
+        self.city_input = QLineEdit(self)
+        self.get_weather_button = QPushButton("Get Weather", self)
+        self.temperature_label = QLabel("Temperature: ", self)  
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    weather_app = weatherApp()
+    weather_app.show()
+    sys.exit(app.exec_())
+
