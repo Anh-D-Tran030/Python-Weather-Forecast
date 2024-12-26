a9df280111d873cd111dd78faea80cb9be9b3132
Anh-D-Tran030 <Anh.D.Tran-2@student.uts.edu.au>
2024-12-26 14:01:22 +1100

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ class weatherApp(QWidget):
         self.city_label = QLabel("Enter city name: ", self )
         self.city_input = QLineEdit(self)
         self.get_weather_button = QPushButton("Get Weather", self)
-        self.temperature_label = QLabel("70°F ", self)  
-        self.emoji_label = QLabel("🌞", self)
-        self.description_label = QLabel("Sunny", self)
+        self.temperature_label = QLabel( self)  
+        self.emoji_label = QLabel(self)
+        self.description_label = QLabel(self)
         self.initUI()
     
     def initUI(self):
@@ -65,8 +65,31 @@ class weatherApp(QWidget):
             font-size: 100px;
             font-family: Segoe UI Emoji;
         }
+        QLabel#description_label {
+            font-size: 50px;
+        }
 
         """)
+
+        self.get_weather_button.clicked.connect(self.get_weather)
+
+    def get_weather(self):
+       
+       api_key = "5fce88621287e63283db9ce43a49910e"
+       city = self.city_input.text()
+       url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
+
+       response = requests.get(url)
+       data =response.json()
+
+       print(data)
+    def display_error(self, message):
+        pass 
+    def display_weather(self, data):
+        pass
+
+
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
